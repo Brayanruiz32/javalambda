fdcf317d58e4d14b5bc7b7ea144b6d62dbb77b9f
Brayanruiz32 <maverick.brm@gmail.com>
2024-06-01 00:22:34 -0500

    levantamiento de la aplicacion

---
+++ src/main/java/com/principal/LambdaBank.java
@@ -0,0 +1,15 @@
+package com.principal;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class LambdaBank implements RequestHandler<BankRequest,BankResponse> {
+
+    @Override
+    public BankResponse handleRequest(BankRequest arg0, Context arg1) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    
+}
